39db6de5a991ec5a469ee098a960dbd92145f313
renxinx <renxinx2021@gmail.com>
2025-04-24 17:17:43 -0700

    run grpo using vllm

---
+++ trl/train_grpo_vllm.py
@@ -0,0 +1,19 @@
+from datasets import load_dataset
+from trl import GRPOConfig, GRPOTrainer
+
+dataset = load_dataset("trl-lib/tldr", split="train")
+dataset = dataset.select(range(int(len(dataset) * 0.01)))  # 选择 1% 的数据
+
+
+# Define the reward function, which rewards completions that are close to 20 characters
+def reward_len(completions, **kwargs):
+    return [-abs(20 - len(completion)) for completion in completions]
+
+training_args = GRPOConfig(output_dir="Qwen2-0.5B-GRPO", logging_steps=10, use_vllm=True)
+trainer = GRPOTrainer(
+    model="Qwen/Qwen2-0.5B-Instruct",
+    reward_funcs=reward_len,
+    args=training_args,
+    train_dataset=dataset,
+)
+trainer.train()
